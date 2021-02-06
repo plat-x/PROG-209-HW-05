52ef89c5ce2fd1323c38bb3c89b1ab153f558a10
plat-x <77313316+plat-x@users.noreply.github.com>
2021-02-06 03:35:46 -0800

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,17 +1,22 @@
-let MovieRating = function(pTitle, pRating) {
-    this.title = pTitle;
-    this.rating = parseInt(pRating);
-}
+// constructor function Movie
+let Movie = function(pTitle, pRating) {
+        // property title
+        this.title = pTitle,
+        // property rating
+        this.rating = parseInt(pRating)
+};
 
-MovieRating.prototype.isValid = function(){
-    if (this.title === "" || this.rating === "" || isNaN(this.rating) || this.rating >5 || this.rating <1) {
-        return false;
-    }
-    else{
-        return true;
-    }
-}
+// validate method to validate title and rating
+Movie.prototype.isValid = function(){
+        // if title is valid and rating is between 1 and 5 return true
+        if (this.title === "" || this.rating === "" || isNaN(this.rating) || this.rating >5 || this.rating <1) {
+                return false;
+        } else {
+                return true;
+        }
+};
 
-MovieRating.prototype.toString = function(){
-    return 'Title: ' + this.title + ' Rating: ' + this.rating;
-}
+// toString method
+Movie.prototype.toString = function(){
+        return 'Title: ' + this.title + ' Rating: ' + this.rating;
+};
